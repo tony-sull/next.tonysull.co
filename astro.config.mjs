6c72200ed90f9e829510934a5c418de173250af8
Tony Sullivan <tony.f.sullivan@outlook.com>
2023-06-13 15:55:01 -0500

    basic header & footer with rel=me links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://next.tonysull.co'
+    site: 'https://next.tonysull.co',
+    experimental: {
+        assets: true
+    },
+    image: {
+        service: sharpImageService()
+    }
 });
