a12877bd1056e55e962d874275208e681b983d94
Tony Sullivan <tony.f.sullivan@outlook.com>
2023-06-14 11:33:42 -0500

    adding basic cards

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,72 +1,74 @@
-import { z } from 'astro/zod'
-import { defineCollection } from 'astro:content'
+import { z } from "astro/zod";
+import { CollectionEntry, defineCollection } from "astro:content";
 
 const baseSchema = z.object({
-    category: z.array(z.string()).optional(),
-    date: z.date({ coerce: true }),
-    'post-status': z.enum(['published', 'draft']).optional(),
-    visibility: z.enum(['public', 'unlisted', 'private']).optional()
-})
+  tags: z.array(z.string()).optional(),
+  date: z.date({ coerce: true }),
+  "post-status": z.enum(["published", "draft"]).optional(),
+  visibility: z.enum(["public", "unlisted", "private"]).optional(),
+});
 
 export function noteSchema() {
-    return baseSchema.extend({
-        'in-reply-to': z.string().url().optional()
-    })
+  return baseSchema.extend({
+    "in-reply-to": z.string().url().optional(),
+  });
 }
 
 export function articleSchema() {
-    return baseSchema.extend({
-        title: z.string()
-    })
+  return baseSchema.extend({
+    title: z.string(),
+  });
 }
 
 export function bookmarkSchema() {
-    return baseSchema.extend({
-        'bookmark-of': z.string().url()
-    })
+  return baseSchema.extend({
+    "bookmark-of": z.string().url(),
+  });
 }
 
 export function rsvpSchema() {
-    return baseSchema.extend({
-        'in-reply-to': z.string().url(),
-        rsvp: z.enum([
-            'yes',
-            'no',
-            'maybe',
-            'interested'
-        ])
-    })
+  return baseSchema.extend({
+    "in-reply-to": z.string().url(),
+    rsvp: z.enum(["yes", "no", "maybe", "interested"]),
+  });
 }
 
 export function likeSchema() {
-    return baseSchema.extend({
-        'like-of': z.string().url()
-    })
+  return baseSchema.extend({
+    "like-of": z.string().url(),
+  });
 }
 
 export function watchSchema() {
-    return baseSchema.extend({
-        'watch-of': z.string().url()
-    })
+  return baseSchema.extend({
+    "watch-of": z.string().url(),
+  });
 }
 
 export const collections = {
-    articles: defineCollection({
-        schema: articleSchema()
-    }),
-    bookmarks: defineCollection({
-        schema: bookmarkSchema()
-    }),
-    likes: defineCollection({
-        schema: likeSchema()
-    }),
-    notes: defineCollection({
-        schema: noteSchema()
-    }),
-    rsvp: defineCollection({
-        schema: rsvpSchema()
-    }),
-    watched: defineCollection({
-        schema: watchSchema()
-    })
-}
+  articles: defineCollection({
+    schema: articleSchema(),
+  }),
+  bookmarks: defineCollection({
+    schema: bookmarkSchema(),
+  }),
+  likes: defineCollection({
+    schema: likeSchema(),
+  }),
+  notes: defineCollection({
+    schema: noteSchema(),
+  }),
+  rsvp: defineCollection({
+    schema: rsvpSchema(),
+  }),
+  watched: defineCollection({
+    schema: watchSchema(),
+  }),
+};
+
+export type Entry =
+  | CollectionEntry<"articles">
+  | CollectionEntry<"bookmarks">
+  | CollectionEntry<"likes">
+  | CollectionEntry<"notes">
+  | CollectionEntry<"rsvp">;
